ca0b4b03199213ee7a2aa89c83a55bf444176ea4
Sergei Tyshchenko <styshchenko4@gmail.com>
2019-05-22 15:57:28 +0600

    Edited code

--- src/components/Row/Cells/Cells.js
+++ src/components/Row/Cells/Cells.js
@@ -2,16 +2,12 @@ import React from 'react';
 import classes from './Cell.module.css';
 
 function Cell(props) {
-  let color = 'cell';
   let value = (props.cellValue === 0) ? '' : (props.cellValue);
-  if (value) {
-    color += ` color-${value}`;
-  }
 
   return (
     <td className={classes.Cell}>
-      <div className={color}>
-        <div className="number">{value}</div>
+      <div className={classes.Cells}>
+        <div className={classes.Number}>{value}</div>
       </div>
     </td>
   );
